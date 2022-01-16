b2e0c0e37e76c86c394b729e87967130a075f618
jacquesjd <Jacques.dancause@gmail.com>
2022-01-15 21:58:16 -0400

    Final changes for local variable assn.

--- tasker.js
+++ tasker.js
@@ -1,4 +1,4 @@
-//this is for a tasker program. It calculates the start date of cycle in ms and today's date in ms.
+//this is for a tasker program. It calculates the start date of cycle in ms 
 
 //cell cycle starts on the following date:
 let dateStarting = 3;
@@ -7,15 +7,17 @@ let date = new Date(Date.now());
 let curYear = date.getFullYear();
 let curMonth = date.getMonth();
 let curDate = date.getDate();
-let todayms = date.getTime();
 let startms;
 
 if(curDate >= dateStarting)
 { //currently in new cycle, this cycle restarts next month on the third
-	startms = new Date(curYear, curMonth, 03).getTime();
+	startms = new Date(curYear, curMonth, 03);
 }
 
 if(curDate < dateStarting)
 { //currently in new month but old cycle, it restarts on the 3rd
-	startms = new Date(curYear - (curMonth > 0 ? 0 : 1), curMonth - (curMonth > 0 ? 1 : -11), dateStarting).getTime();
+	startms = new Date(curYear - (curMonth > 0 ? 0 : 1), curMonth - (curMonth > 0 ? 1 : -11), dateStarting);
 }
+
+//returning local variables
+var startDateIso = startms.toISOString();
